44f8bb246edcdb0148a186494fb2fab26e215cec
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2024-06-30 16:22:51 -0700

    markdown for ul and ol list

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -174,7 +174,6 @@ export default function Chat() {
                       p: ({ node, ...props }) => (
                         <p className="whitespace-pre-wrap" {...props} />
                       ),
-
                       code: ({
                         node,
                         //@ts-ignore
@@ -198,6 +197,24 @@ export default function Chat() {
                           </code>
                         );
                       },
+                      ul: ({ node, ...props }) => (
+                        <ul
+                          className="-mt-1 list-disc space-y-2 pl-8"
+                          {...props}
+                        />
+                      ),
+                      ol: ({ node, ...props }) => (
+                        <ol
+                          className="-mt-1 list-decimal space-y-2 pl-8"
+                          {...props}
+                        />
+                      ),
+                      li: ({ node, ...props }) => (
+                        <li
+                          className="whitespace-normal break-words"
+                          {...props}
+                        />
+                      ),
                     }}
                   >
                     {message.content}
