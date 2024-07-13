d32479afd1f596d51f326f3cc894bf4f6b2e6d71
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2024-07-13 12:40:09 -0700

    fix auth callback route

--- src/app/(auth)/callback/route.ts
+++ src/app/(auth)/callback/route.ts
@@ -4,11 +4,14 @@ import { NextRequest } from 'next/server';
 import { getErrorRedirect, getStatusRedirect } from '@/utils/helpers';
 
 export async function GET(request: NextRequest) {
+  // The `/auth/callback` route is required for the server-side auth flow implemented
+  // by the `@supabase/ssr` package. It exchanges an auth code for the user's session.
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
 
   if (code) {
     const supabase = createClient();
+
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (error) {
@@ -22,50 +25,12 @@ export async function GET(request: NextRequest) {
     }
   }
 
-  const supabase = createClient();
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    return NextResponse.redirect(
-      getErrorRedirect(
-        `${requestUrl.origin}/signin`,
-        'No user',
-        "Sorry, we weren't able to log you in. Please try again."
-      )
-    );
-  }
-
-  // Check if user is onboarded
-  let { data: userprofiles, error } = await supabase
-    .from('userprofiles')
-    .select('bio')
-    .eq('userid', user.id);
-
-  if (error || !userprofiles || userprofiles.length === 0) {
-    return NextResponse.redirect(
-      getErrorRedirect(
-        `${requestUrl.origin}/signin`,
-        'Profile Error',
-        "Sorry, we weren't able to retrieve your profile. Please try again."
-      )
-    );
-  }
-
-  const userProfile = userprofiles[0];
-
-  if (userProfile.bio) {
-    return NextResponse.redirect(
-      `${requestUrl.origin}`
-    );
-  } else {
-    return NextResponse.redirect(
-      getStatusRedirect(
-        `${requestUrl.origin}`,
-        'Success!',
-        'You are now signed in.'
-      )
-    );
-  }
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(
+    getStatusRedirect(
+      `${requestUrl.origin}/account`,
+      'Success!',
+      'You are now signed in.'
+    )
+  );
 }
